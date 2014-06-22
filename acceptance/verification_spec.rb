cbd1d4b0c80fe2843a794a82e5c36a5d486ddd4f
Jake Goulding <jake.goulding@gmail.com>
2014-06-22 18:42:22 -0400

    Split big acceptance tests into multiple tests
    In addition, add some test variables and helper methods.

--- acceptance/verification_spec.rb
+++ acceptance/verification_spec.rb
@@ -3,36 +3,99 @@ require 'rexml/document'
 REPORTS_DIR = File.dirname(__FILE__) + '/reports'
 
 describe "MiniTest::Unit acceptance" do
-  it "should generate two XML files" do
-    File.exist?(File.join(REPORTS_DIR, 'TEST-MiniTestExampleTestOne.xml')).should == true
-    File.exist?(File.join(REPORTS_DIR, 'TEST-MiniTestExampleTestTwo.xml')).should == true
+  let(:report_path_1) {File.join(REPORTS_DIR, 'TEST-MiniTestExampleTestOne.xml')}
+  let(:report_path_2) {File.join(REPORTS_DIR, 'TEST-MiniTestExampleTestTwo.xml')}
+
+  context "generated files" do
+    it "generates two XML files" do
+      pending "A rogue file is created"
+      expect(Dir["#{REPORTS_DIR}/*.xml"].count).to eql 2
+    end
+
+    it "generates XML for the first test" do
+      expect(File.exist?(report_path_1)).to be true
+    end
+
+    it "generates XML for the second test" do
+      expect(File.exist?(report_path_2)).to be true
+    end
   end
 
-  it "should have one error and one failure for MiniTestExampleTestOne" do
-    doc = File.open(File.join(REPORTS_DIR, 'TEST-MiniTestExampleTestOne.xml')) do |f|
-      REXML::Document.new(f)
+  context "a test with a failure and an error" do
+    let (:root) { load_xml_result(report_path_1).root }
+
+    it "has one error" do
+      expect(root.attributes["errors"]).to eql "1"
     end
-    doc.root.attributes["errors"].should == "1"
-    doc.root.attributes["failures"].should == "1"
-    doc.root.attributes["assertions"].should == "1"
-    doc.root.attributes["tests"].should == "1"
-    doc.root.elements.to_a("/testsuite/testcase").size.should == 1
-    doc.root.elements.to_a("/testsuite/testcase/error").size.should == 1
-    doc.root.elements.to_a("/testsuite/testcase/failure").size.should == 1
-    doc.root.elements.to_a("/testsuite/system-out").first.texts.inject("") do |c,e|
-      c << e.value; c
-    end.strip.should == "Some <![CDATA[on stdout]]>"
+
+    it "has one failure" do
+      expect(root.attributes["failures"]).to eql "1"
+    end
+
+    it "has one assertion" do
+      expect(root.attributes["assertions"]).to eql "1"
+    end
+
+    it "has one test" do
+      expect(root.attributes["tests"]).to eql "1"
+    end
+
+    it "has one testcase" do
+      expect(root.elements.to_a("/testsuite/testcase").size).to eql 1
+    end
+
+    context "the testcase" do
+      it "has an error" do
+        expect(root.elements.to_a("/testsuite/testcase/error").size).to eql 1
+      end
+
+      it "has a failure" do
+        expect(root.elements.to_a("/testsuite/testcase/failure").size).to eql 1
+      end
+    end
+
+    it "captures the STDOUT" do
+      content = root.elements.to_a("/testsuite/system-out").first.texts.inject("") do |c,e|
+        c << e.value; c
+      end.strip
+
+      expect(content).to eql "Some <![CDATA[on stdout]]>"
+    end
   end
 
-  it "should have no errors or failures for MiniTestExampleTestTwo" do
-    doc = File.open(File.join(REPORTS_DIR, 'TEST-MiniTestExampleTestTwo.xml')) do |f|
+  context "a test without failures" do
+    let(:root) { load_xml_result(report_path_2).root }
+
+    it "has zero errors" do
+      expect(root.attributes["errors"]).to eql "0"
+    end
+
+    it "has zero failures" do
+      expect(root.attributes["failures"]).to eql "0"
+    end
+
+    it "has one assertion" do
+      expect(root.attributes["assertions"]).to eql "1"
+    end
+
+    it "has one test" do
+      expect(root.attributes["tests"]).to eql "1"
+    end
+
+    it "has one testcase" do
+      expect(root.elements.to_a("/testsuite/testcase").size).to eql 1
+    end
+
+    context "the testcase" do
+      it "has no failures" do
+        expect(root.elements.to_a("/testsuite/testcase/failure").size).to eql 0
+      end
+    end
+  end
+
+  def load_xml_result(path)
+    File.open(path) do |f|
       REXML::Document.new(f)
     end
-    doc.root.attributes["errors"].should == "0"
-    doc.root.attributes["failures"].should == "0"
-    doc.root.attributes["assertions"].should == "1"
-    doc.root.attributes["tests"].should == "1"
-    doc.root.elements.to_a("/testsuite/testcase").size.should == 1
-    doc.root.elements.to_a("/testsuite/testcase/failure").size.should == 0
   end
 end
